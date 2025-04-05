aa988a8df2d71936d939983c5eb8c7162113be68
Ruslan <info.sionbaltic.eu>
2025-04-05 20:31:52 +0300

    Commit N2

--- src/Java_Open.java
+++ src/Java_Open.java
@@ -2,6 +2,6 @@ public class Java_Open {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Second line");
-        NewClass2 newClass2 = new NewClass2(1,"sss");
+        NewClass2 newClass2 = new NewClass2(1,"sss","dddd");
     }
 }
--- src/NewClass2.java
+++ src/NewClass2.java
@@ -1,11 +1,23 @@
 public class NewClass2 {
     int a;
     String b;
+    String c;
 
-    public NewClass2(int a, String b) {
+    public NewClass2(int a, String b, String c) {
         this.a = a;
         this.b = b;
+        this.c = c;
     }
 
+    public int getA() {
+        return a;
+    }
 
+    public String getB() {
+        return b;
+    }
+
+    public String getC() {
+        return c;
+    }
 }

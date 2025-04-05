9f005d2b5f5d539d32ca509f23b9dfccd9dc1ba7
Ruslan <info.sionbaltic.eu>
2025-04-05 20:19:35 +0300

    Commit N1

---
+++ src/Java_Open.java
@@ -0,0 +1,7 @@
+public class Java_Open {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Second line");
+        NewClass2 newClass2 = new NewClass2(1,"sss");
+    }
+}
---
+++ src/NewClass2.java
@@ -0,0 +1,11 @@
+public class NewClass2 {
+    int a;
+    String b;
+
+    public NewClass2(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+
+}
